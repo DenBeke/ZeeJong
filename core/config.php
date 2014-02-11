b9302fcc032f95b0b16cf148e9c15d6ace232960
MathiasB <bekemathias@gmail.com>
2014-02-11 19:02:25 +0100

    Added config file and gitignore

---
+++ core/config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+General configuration
+
+Created: February 2014
+*/
+
+//Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'BettingSystem');
+
+
+//General site url (with slash at the end)
+define('SITE_URL', 'http://localhost:8888/');
+
+?>
