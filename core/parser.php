e81c71a4450b18f81983333d33d62b8186aac403
MathiasB <bekemathias@gmail.com>
2014-02-13 15:03:36 +0100

    Implemented parseMatch

--- core/parser.php
+++ core/parser.php
@@ -37,6 +37,8 @@ function parse($url) {
 		//Still a bug at the end when parsing this page:
 		// http://int.soccerway.com/international/world/world-cup/2010-south-africa/s4770/final-stages/
 
+		break;
+
 	}
 	
 }
@@ -58,24 +60,41 @@ function parseMatch($url) {
 	
 	
 	//Find the goals
-	$goals = $html->find('.block_match_goals', 0);
-	foreach ($goals->find('.player') as $player) {
+	$rawGoals = $html->find('.block_match_goals', 0);
+	$goals = array();
+	foreach ($rawGoals->find('.player') as $player) {
 		$playerName = $player->find('a', 0)->plaintext;
 		$time = $player->find('.minute', 0)->plaintext;
-		echo "--- Goal: $playerName ($time)<br>";
+		if($time != '') {
+			$goals[intval($time)] = $playerName;
+		}
+	}
+
+	foreach ($goals as $a => $b) {
+		echo "--- Goal: $b ($a')<br>";
 	}
 	
 	
 	//Find the referee
-	
+	$referee = $html->find('.referee', 0)->plaintext;
+	echo "--- Referee: $referee<br>";
 	
 	//Find the date
-	
+	$date = $html->find('.middle .details dd', 1)->plaintext;
+	echo "--- Date: $date<br>";
 	
 	//Find the final score
+	$score = $html->find('#subheading .bidi', 0)->plaintext;
+	$score = explode(' - ', $score);
+	
+	$scoreA = intval($score[0]);
+	$scoreB = intval($score[1]);
+		
+	echo "--- Score: $scoreA vs $scoreB<br>";
 	
 	
 	//Find the place where the match took place
+	
 	
 	
 	$html->clear(); //Clear DOM tree (memory leak in simple_html_dom)
