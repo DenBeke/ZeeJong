4919717cf52a917fe4fb18404cfaf57817fe83d1
Noyb Cybot <alkaloid.btx@gmail.com>
2014-02-14 21:47:02 +0100

    Finished refactoring

---
+++ install/form.tpl.php
@@ -0,0 +1,19 @@
+<form method="post" class="pure-form">
+	
+	<fieldset class="pure-group">
+		
+		<input type="text" name="host" class="pure-input-1-2" placeholder="host">
+		<input type="text" name="port" class="pure-input-1-2" placeholder="port">
+		<input type="text" name="database" class="pure-input-1-2" placeholder="database name" required="required">
+		
+	</fieldset>
+		
+	<fieldset class="pure-group">
+		
+		<input type="text" name="username" class="pure-input-1-2" placeholder="username" required="required">
+		<input type="password" name="password" class="pure-input-1-2" placeholder="password" required="required">
+		
+	</fieldset>
+	
+	<input type="submit" name="submit" value="Submit" class="pure-button pure-input-1-2 pure-button-primary">
+</form>
