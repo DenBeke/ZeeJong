833f14f3169d481f4925f23da2d5f9469f7b6027
Noyb Cybot <noyb@cybot.com>
2014-02-16 21:20:55 +0100

    Added comment

--- install/config.php
+++ install/config.php
@@ -1,4 +1,6 @@
 <?php
+// Add keys and values to the config variable to add them to the installation
+
 $config['SITE_URL'] = 'sprintf(\'%s://%s%s\', isset($_SERVER[\'HTTPS\']) && $_SERVER[\'HTTPS\'] != \'off\' ? \'https\' : \'http\', $_SERVER[\'HTTP_HOST\'], $_SERVER[\'REQUEST_URI\'])';
 ?>
 
