d737bfd0988fe1f540b22b08b4f808903ac2515e
eli <eliasvanlangenhove@hotmail.com>
2014-02-17 22:49:51 +0100

    Added several inserts, will be completed tomorrow

---
+++ core/classes/Coaches.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Coaches Class
+*/
+
+
+/**
+@brief Coaches Class
+
+The class contains the following information:
+- id
+- teamId
+- coachId
+- date
+*/
+class Coaches {
+	private $id;
+	private $teamId;
+	private $coachId;
+	private $date;
+
+	/**
+	Constructor
+	@param id
+	*/
+	public function __construct($id) {
+		$this->id = $id;
+	}
+
+	/**
+	Get the ID of the coach
+	@return id
+	*/
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	String function
+	@return string
+	*/
+	public function __toString() {
+		return "ID: $this->id";
+	}
+
+}
