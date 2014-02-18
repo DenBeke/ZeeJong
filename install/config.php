341783666bb17bd62ab4545095fd775fa4eb78cb
noyb cybot <dark_array@hotmail.com>
2014-02-18 10:39:06 +0000

    config.php edited online with Bitbucket

--- install/config.php
+++ install/config.php
@@ -1,7 +1,45 @@
 <?php
 // Add keys and values to the config variable to add them to the installation
 
-$config['SITE_URL'] = 'sprintf(\'%s://%s%s\', isset($_SERVER[\'HTTPS\']) && $_SERVER[\'HTTPS\'] != \'off\' ? \'https\' : \'http\', $_SERVER[\'HTTP_HOST\'], $_SERVER[\'REQUEST_URI\'])';
+function get_base_url()
+{
+	/* First we need to get the protocol the website is using */
+	$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"], 0, 5)) == 'https' ? 'https://' : 'http://';
+	
+	/* returns /myproject/index.php */
+	$path = $_SERVER['PHP_SELF'];
+	
+	/*
+	* returns an array with:
+	* Array (
+	*  [dirname] => /myproject/
+	*  [basename] => index.php
+	*  [extension] => php
+	*  [filename] => index
+	* )
+	*/
+	$path_parts = pathinfo($path);
+	$directory = $path_parts['dirname'];
+	/*
+	* If we are visiting a page off the base URL, the dirname would just be a "/",
+	* If it is, we would want to remove this
+	*/
+	$directory = ($directory == "/") ? "" : $directory;
+		
+	/* Returns localhost OR mysite.com */
+	$host = $_SERVER['HTTP_HOST'];
+	
+	/*
+	* Returns:
+	* http://localhost/mysite
+	* OR
+	* https://mysite.com
+	*/
+	return $protocol . $host . $directory;
+}
+
+$config['SITE_URL'] = '\'' . get_base_url . '\'';
+
 ?>
 
 
