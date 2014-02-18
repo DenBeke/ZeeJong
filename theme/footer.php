c93da05f6656da998f11f0f1dad435059c33e0a7
MathiasB <bekemathias@gmail.com>
2014-02-18 17:03:22 +0100

    Added Bootstrap and JQuery

--- theme/footer.php
+++ theme/footer.php
@@ -5,5 +5,14 @@ Template part for footer
 Created: February 2014
 */
 ?>
+
+	</div>
+
+	<div id="footer">
+	  <div class="container">
+		<p class="muted credit">&copy;2014 ZeeJong Betting System</p>
+	  </div>
+	</div>
+
 </body>
 </html>
