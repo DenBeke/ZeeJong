c6d4515e73c36ca6c3f60df5dc78128f01502cba
Noyb Cybot <noyb@cybot.be>
2014-02-27 14:20:38 +0100

    Refactored competition error

--- core/controller/Error.php
+++ core/controller/Error.php
@@ -12,28 +12,12 @@ require_once(dirname(__FILE__) . '/Controller.php');
 
 
 	class Error extends Controller {
-	
-	
-		private $template = 'error.php';
+
 		public $page = 'error';
 	
 	
-		/**
-		Render the template part of the view
-		
-		@exception theme file does not exist
-		*/
-		public function template() {
-			
-			if(is_array($this->data)) {
-				extract($this->data);
-			}
-			
-			if(file_exists($this->themeDir . '/' . $this->template)) {
-				include($this->themeDir . '/' . $this->template);
-			}
-			
-		
+		public function __construct() {
+			$this->theme = 'error.php';
 		}
 		
 		
