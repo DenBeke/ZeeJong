68358b7700c9613ce60d97b09d5a6da73d6d1b47
Noyb Cybot <noyb@cybot.be>
2014-02-27 14:18:38 +0100

    Refactored coach

--- core/controller/Coach.php
+++ core/controller/Coach.php
@@ -12,28 +12,12 @@ require_once(dirname(__FILE__) . '/Controller.php');
 
 
 	class Coach extends Controller {
-	
-	
-		private $template = 'coach.php';
+
 		public $page = 'coach';
 	
 	
-		/**
-		Render the template part of the view
-		
-		@exception theme file does not exist
-		*/
-		public function template() {
-			
-			if(is_array($this->data)) {
-				extract($this->data);
-			}
-			
-			if(file_exists($this->themeDir . '/' . $this->template)) {
-				include($this->themeDir . '/' . $this->template);
-			}
-			
-		
+		public function __construct() {
+			$this->theme = 'coach.php';
 		}
 		
 		
