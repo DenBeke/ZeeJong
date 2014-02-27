235d4d867eb36179314be6e7cecee6592aefb0da
Alexander <liopleurodon@bitbucket.org>
2014-02-27 10:03:46 +0100

    Fixed registration form

--- core/controller/Register.php
+++ core/controller/Register.php
@@ -42,7 +42,19 @@ namespace Controller {
 		 * Constructor
 		 */
 		public function __construct() {
-			$db = new \Database;
+			if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['password2']) || !isset($_POST['email'])) {
+				exit();
+			} else {
+
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+				$password2 = $_POST['password2'];
+
+				$emailAddress = $_POST['email'];
+
+				$this -> register($username, $password, $password2, $emailAddress);
+
+			}
 		}
 
 		/**
@@ -56,46 +68,32 @@ namespace Controller {
 		 * Register a user
 		 */
 		public function register($username, $password, $password2, $emailAddress) {
+			$database = new \Database;
 			// Test if logged in
 			if (isset($_SESSION['userID']) and $database -> doesUserExist($_SESSION['userID'])) {
-				$this -> registerMessage = 'You are already logged in.';
+				$this -> registerMessage = '	<div class="alert alert-danger"><strong>You are already logged in.</strong></div>';
 				return false;
 			}
 
 			// Test if username already exists
-			if ($db -> doesUserExist($username)) {
-				$this -> registerMessage = 'Username already exists, please choose a different one.';
+			if ($database -> doesUserNameExist($username)) {
+				$this -> registerMessage = '<div class="alert alert-danger"><strong>Username already exists, please choose a different one.</strong></div>';
 				return false;
 			}
 
 			// Test if passwords are the same
 			if ($password != $password2) {
-				$this -> registerMessage = 'Passwords do not match.';
+				$this -> registerMessage = '<div class="alert alert-danger"><strong>Passwords do not match.</strong></div>';
 				return false;
 			}
 
-			$salt = generateSalt();
+			$salt = $this -> generateSalt();
 			$hashedPassword = hashPassword($password, $salt);
-			$id = $db -> registerUser($username, $salt, $hashedPassword, $emailAddress);
-			$this -> registerMessage = 'Congratulations, account was successfully created.';
+			$id = $database -> registerUser($username, $salt, $hashedPassword, $emailAddress);
+			$this -> registerMessage = '<div class="alert alert-success">Congratulations, account was successfully created.</strong></div>';
 		}
 
 	}
-/*
-	if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['password2']) || !isset($_POST['email'])) {
 
-	} else {
-
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$password2 = $_POST['password2'];
-
-		$emailAddress = $_POST['email'];
-
-		$this -> registerUser($username, $password, $password2, $emailAddress);
-
-	}
-*/
- 
 }
 ?>
