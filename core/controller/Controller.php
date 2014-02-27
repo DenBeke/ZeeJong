8baccd9b5a09f692a2faaa85707ae426f0404870
noyb cybot <dark_array@hotmail.com>
2014-02-27 13:05:28 +0000

    Added template code in base class controller

--- core/controller/Controller.php
+++ core/controller/Controller.php
@@ -20,10 +20,18 @@ require_once(dirname(__FILE__) . '/../config.php');
 				
 		/**
 		Render the template part of the view
-	
+		
 		@exception theme file does not exist
 		*/
-		abstract public function template();
+		public function template() {
+			if(is_array($this->data)) {
+				extract($this->data);
+			}
+			
+			if(file_exists($this->themeDir . '/' . $this->template)) {
+				include($this->themeDir . '/' . $this->template);
+			}	
+		}
 		
 		
 		/**
@@ -48,12 +56,7 @@ require_once(dirname(__FILE__) . '/../config.php');
 				$this->data = $args;
 			}
 		}
-	
-	
-	
 		
-	
-	
 	}
 
 
