212b32d5d3d0df5cbb58bee32764130536d6a86b
Noyb Cybot <noyb@cybot.be>
2014-02-27 14:26:09 +0100

    Refactored tournament

--- core/controller/Tournament.php
+++ core/controller/Tournament.php
@@ -13,28 +13,10 @@ require_once(dirname(__FILE__) . '/Controller.php');
 
 	class Tournament extends Controller {
 	
-	
-		private $template = 'tournament.php';
 		public $page = 'tournament';
 	
-	
-		/**
-		Render the template part of the view
-		
-		@exception theme file does not exist
-		*/
-		public function template() {
-			
-			if(is_array($this->data)) {
-				extract($this->data);
-			}
-			
-			if(file_exists($this->themeDir . '/' . $this->template)) {
-				include($this->themeDir . '/' . $this->template);
-			}
-			
-		
-		}
+		public function __construct() {
+			$this->theme = 'tournament.php';
 		
 		
 		/**
