5630e14358b581535c4c04171d30f0e5c8ef9477
Noyb Cybot <noyb@cybot.be>
2014-02-27 14:24:52 +0100

    Refactored referee

--- core/controller/Referee.php
+++ core/controller/Referee.php
@@ -13,27 +13,10 @@ require_once(dirname(__FILE__) . '/Controller.php');
 
 	class Referee extends Controller {
 	
-	
-		private $template = 'referee.php';
 		public $page = 'referee';
 	
-	
-		/**
-		Render the template part of the view
-		
-		@exception theme file does not exist
-		*/
-		public function template() {
-			
-			if(is_array($this->data)) {
-				extract($this->data);
-			}
-			
-			if(file_exists($this->themeDir . '/' . $this->template)) {
-				include($this->themeDir . '/' . $this->template);
-			}
-			
-		
+		public function __construct() {
+			$this->theme = 'referee.php';
 		}
 		
 		
