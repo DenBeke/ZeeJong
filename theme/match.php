a5f702c0d08a7b11bda0997d6ce262a1f77b2172
MathiasB <Mathias@Mac-Pro-van-Mathias-2.local>
2014-03-04 18:18:57 +0100

    Implemented view for match

--- theme/match.php
+++ theme/match.php
@@ -5,44 +5,50 @@ Template part for match page
 Created: February 2014
 */
 ?>
-<h2 id="title-match"><?php echo $object->getTeamA()->getName() ?> - <?php echo $object->getTeamB()->getName() ?></h2>
 
 
-<h3><?php echo $object->getTeamA()->getName() ?></h3>
+<div class="container">
 
-<ul class="team-a">
+	<h2 id="title-match"><?php echo $this->match->getTeamA()->getName() ?> - <?php echo $this->match->getTeamB()->getName() ?></h2>
+	
+	
+	<h3><?php echo $this->match->getTeamA()->getName() ?></h3>
+	
+	<ul class="team-a">
+	
+		<?php
+		foreach($this->match->getPlayersTeamA() as $player) { 
+		?>
+	
+		<li>
+			<a href="<?php echo SITE_URL . 'player/' . $player->getId(); ?>"><?php echo $player->getName(); ?></a>
+		</li>
+	
+		<?php
+		} //end foreach
+		?>
+	
+	</ul>
+	
+	
+	
+	
+	<h3><?php echo $this->match->getTeamB()->getName() ?></h3>
+	
+	<ul class="team-a">
+	
+		<?php
+		foreach($this->match->getPlayersTeamB() as $player) { 
+		?>
+	
+		<li>
+			<a href="<?php echo SITE_URL . 'player/' . $player->getId(); ?>"><?php echo $player->getName(); ?></a>
+		</li>
+	
+		<?php
+		} //end foreach
+		?>
+	
+	</ul>
 
-	<?php
-	foreach($object->getPlayersTeamA() as $player) { 
-	?>
-
-	<li>
-		<a href="<?php echo SITE_URL . '?page=player&player=' . $player->getId(); ?>"><?php echo $player->getName(); ?></a>
-	</li>
-
-	<?php
-	} //end foreach
-	?>
-
-</ul>
-
-
-
-
-<h3><?php echo $object->getTeamB()->getName() ?></h3>
-
-<ul class="team-a">
-
-	<?php
-	foreach($object->getPlayersTeamB() as $player) { 
-	?>
-
-	<li>
-		<a href="<?php echo SITE_URL . '?page=player&player=' . $player->getId(); ?>"><?php echo $player->getName(); ?></a>
-	</li>
-
-	<?php
-	} //end foreach
-	?>
-
-</ul>
+</div>
