4b4b22ed91b380c2a4064a28960ff50abaa66178
MathiasB <bekemathias@gmail.com>
2014-03-04 13:53:22 +0100

    Created view for Player

--- theme/player.php
+++ theme/player.php
@@ -5,42 +5,38 @@ Template part for player page
 Created: February 2014
 */
 ?>
-<div class="content">
+<div class="container">
 
-	<h2 id="title-player">Vincent Kompany</h2>
+	<h2 id="title-player"><?php echo $this->player->getName(); ?></h2>
 	
 	
 	<ul class="list-group player-meta">
 	
 	  <li class="list-group-item">
-	  	First name: Vincent
+	  	First name: <?php echo $this->player->getFirstName(); ?>
 	  </li>
 	  
 	  <li class="list-group-item">
-	  	Last name: Kompany
+	  	Last name: <?php echo $this->player->getLastName(); ?>
 	  </li>
 	  
 	  <li class="list-group-item">
-	  	First name: Vincent
-	  </li>
-	  
-	  <li class="list-group-item">
-	  	Nationality: Belgium
+	  	Nationality: <?php echo $this->player->getCountry()->getName(); ?>
 	  </li>
 	
 	  <li class="list-group-item">
-	    <span class="badge">14</span>
+	    <span class="badge"><?php echo $this->player->getTotalNumberOfGoals(); ?></span>
 	    Goals
 	  </li>
 	  
 	  <li class="list-group-item">
-	    <span class="badge">4</span>
+	    <span class="badge"><?php echo $this->player->getTotalNumberOfCards(); ?></span>
 	    Yellow Cards
 	  </li>
 	  
 	  <li class="list-group-item">
-	    <span class="badge">1</span>
-	    Red Cards
+	    <span class="badge"><?php echo $this->player->getTotalNumberOfMatches(); ?></span>
+	    Matches Played
 	  </li>
 	</ul>
 
