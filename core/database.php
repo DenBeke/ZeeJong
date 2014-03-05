9ccf40fcbabc040184e96985b38a44e3435652b3
Noyb Cybot <noyb@cybot.com>
2014-03-05 20:17:09 +0100

    Added includes

--- core/database.php
+++ core/database.php
@@ -6,6 +6,8 @@
 */
 
 require_once(dirname(__FILE__) . '/config.php');
+require_once(dirname(__FILE__) . '/Selector.php');
+require_once(dirname(__FILE__) . '/functions.php');
 require_once(dirname(__FILE__) . '/classes/Bet.php');
 require_once(dirname(__FILE__) . '/classes/Card.php');
 require_once(dirname(__FILE__) . '/classes/Coach.php');
