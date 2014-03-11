305ec1afb7c99e325aac201eb6308353b2f61afb
Alexander <liopleurodon@bitbucket.org>
2014-03-11 19:26:27 +0100

    Continued work on bets; bets still have to be added manually into database

--- theme/bets.php
+++ theme/bets.php
@@ -1,8 +1,45 @@
 <?php
 /*
-Template part all bets page
-This page displays the bets for all matches
+ Template part all bets page
+ This page displays the bets for all matches
 
-Created: March 2014
-*/
+ Created: March 2014
+ */
+?>
+<div class="container">
+	<?php
+		if(!loggedIn()){
+		// User is not logged in
+	?>
+	<div class="alert alert-danger">
+		<strong>You are not logged in.</strong>
+	</div>
+	<?php
+	}else{
+	?>
+	<h2>Your bets</h2>
+	<table class="table table-striped">
+		<tr>
+			<th>Match</th>
+			<th>Team</th>
+			<th>Amount</th>
+		</tr>
+	<?php
+
+		global $database;
+		$bets = $this->getBets();
+		foreach($bets as $betId) {
+		$bet = new Bet($betId,$database);
+	?>
+		<tr>
+			<td><?php echo $bet->getMatchId() ?></td>
+			<td><?php echo $bet->getTeamId() ?></td>
+			<td><?php echo $bet->getMoney() ?></td>
+		</tr>
+	<?php
+	}
+	}
+	?>
+		</table>
+</div>
 
--- theme/header.php
+++ theme/header.php
@@ -69,7 +69,13 @@
 				        </ul>
 				        
 				      </li>
+				    <?php
+					if (loggedIn()) {
+					?>
 					<li><a href="<?php echo SITE_URL; ?>bets">Bets</a></li>
+					<?php
+					}
+					?>
 				    </ul>
 		         
 				       <?php
