02d7fa17c8b7b74dad081ef2b1c5842aac5dd566
MathiasB <bekemathias@gmail.com>
2014-03-12 10:56:47 +0100

    View for referee

--- theme/referee.php
+++ theme/referee.php
@@ -8,6 +8,47 @@ Created: February 2014
 
 <div class="container">
 
-	<h2 id="title-referee"><?php echo $this->referee->getName(); ?></h2>
+	<div class="row">
+	
+		
+	
+			<div class="container">
+				<h2 id="title-referee"><?php echo $this->referee->getName(); ?></h2>
+			</div>
+			
+			<div class="col-md-10">
+			
+			
+			<ul class="list-group player-meta">
+			
+			  <li class="list-group-item">
+			  	<b>First name</b>: <?php echo $this->referee->getFirstName(); ?>
+			  </li>
+			  
+			  <li class="list-group-item">
+			  	<b>Last name</b>: <?php echo $this->referee->getLastName(); ?>
+			  </li>
+			  
+			  <li class="list-group-item">
+			  	<b>Nationality</b>: <?php echo $this->referee->getCountry()->getName(); ?>
+			  </li>
+			 
+			   
+			</ul>
+		
+		</div>
+		
+		
+		<div class="col-md-2 image-container">
+		
+		
+			<img src="<?php echo SITE_URL . 'core/cache/Referee-' . $this->referee->getId() . '.png'; ?>">
+			
+		
+		</div>
+	
+	
+	</div>
+
 
 </div>
