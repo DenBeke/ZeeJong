9dfc0612f3d0bc4b7a627c5a7910a9ea5af0332c
Alexander <liopleurodon@bitbucket.org>
2014-03-13 19:32:11 +0100

    Allow placement of bets through $_GET

--- core/controller/placeBet.php
+++ core/controller/placeBet.php
@@ -12,45 +12,82 @@ namespace Controller {
 
 	class placeBet extends Controller {
 
-		public $page = 'placeBet';
-		private $template = 'placeBet.php';
-		public $bet;
-		public $betSuccessMessage;
-		public $betErrorMessage;
-
-		/**
-		 Render the template part of the view
-
-		 @exception theme file does not exist
-		 */
-		public function template() {
-
-			if (is_array($this -> data)) {
-				extract($this -> data);
-			}
-
-			if (file_exists($this -> themeDir . '/' . $this -> template)) {
-				include ($this -> themeDir . '/' . $this -> template);
-			}
-
-		}
+		private $bet;
+		private $betSuccessMessage;
+		private $betErrorMessage;
+		private $matchId;
 
 		public function __construct() {
 			global $database;
+			$this -> theme = 'placeBet.php';
 			// Test if logged in
-			if (!isset($_SESSION['userID']) || !isset($_POST['matchId']) || !isset($_POST['score1']) || !isset($_POST['score2']) || !isset($_POST['money'])) {
+			if (!isset($_SESSION['userID']) || !isset($_POST['score1']) || !isset($_POST['score2']) || !isset($_POST['money']) || !isset($_POST['matchId'])) {
 				return;
 			}
 			if (!$database -> doesUserExist($_SESSION['userID'])) {
 				return;
 			}
-			$this->placeBet();
+			if (!$_POST['money'] > 0) {
+				$this -> betErrorMessage = $this -> betErrorMessage . "You must bet for more than €0." . "\r\n";
+				$this -> matchId = $_POST['matchId'];
+			}
+			if ($_POST['score1'] < 0 || $_POST['score2'] < 0) {
+				$this -> betErrorMessage = $this -> betErrorMessage . "Scores can't be less than 0." . "\r\n";
+				$this -> matchId = $_POST['matchId'];
+			}
+			if (strlen($this -> betErrorMessage) == 0) {
+				// Safe to place bet
+				$this -> placeBet();
+			}
 		}
 
 		private function placeBet() {
 			global $database;
 			$database -> addBet($_POST['matchId'], $_POST['score1'], $_POST['score2'], $_SESSION['userID'], $_POST['money']);
 			$this -> betSuccessMessage = $this -> betSuccessMessage . "Bet was successfully placed." . "\r\n";
+		}
+
+		/**
+		 Call GET methode with parameters
+
+		 @param params
+		 */
+		public function GET($args) {
+			if (!isset($args[1])) {
+				throw new \exception('No match id given');
+				return;
+			}
+			$this -> args = $args[1];
+			global $database;
+			if (!$database -> doesMatchExist($args[1])) {
+				$this -> betErrorMessage = $this -> betErrorMessage . "Match does not exist." . "\r\n";
+				return;
+			}
+			$this -> matchId = $args[1];
+		}
+
+		/**
+		 Get the error message
+		 @return the error message (if one exists)
+		 */
+		public function getErrorMessage() {
+			return $this -> betErrorMessage;
+		}
+
+		/**
+		 Get the success message
+		 @return the success message (if one exists)
+		 */
+		public function getSuccessMessage() {
+			return $this -> betSuccessMessage;
+		}
+
+		/**
+		 Get the match
+		 @return the match object
+		 */
+		public function getMatchId() {
+			return $this -> matchId;
 		}
 
 	}
