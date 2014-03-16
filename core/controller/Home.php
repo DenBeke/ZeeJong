aec4d4b76e7d5adc6ce957a74d20d1a28c3c57c5
MathiasB <bekemathias@gmail.com>
2014-03-16 15:14:34 +0100

    Added top players to homepage

--- core/controller/Home.php
+++ core/controller/Home.php
@@ -15,11 +15,13 @@ require_once(dirname(__FILE__) . '/Controller.php');
 	
 		public $page = 'home';
 		public $events;
+		public $players;
 	
 		public function __construct() {
 			global $database;
 			$this->theme = 'home.php';
-			$this->events = $database->getUpcommingEvents(6);
+			$this->events = $database->getUpcommingEvents(5);
+			$this->players = $database->getPlayersWithMostMatches(0,6);
 		}
 		
 		
