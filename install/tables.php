03ebdc7ba70a1844f5fbd304bcbd3ed2e8f566ff
Alexander <liopleurodon@bitbucket.org>
2014-03-29 10:59:00 +0100

    Added group tables to install script

--- install/tables.php
+++ install/tables.php
@@ -177,6 +177,24 @@ CREATE TABLE IF NOT EXISTS `User` (
 `money` int(11) DEFAULT "0",
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB ;
+',
+
+'
+CREATE TABLE IF NOT EXISTS `Group`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(50) NOT NULL,
+`ownerId` int(11) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB ;
+',
+
+'
+CREATE TABLE IF NOT EXISTS `GroupMembership` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`userId` int(11) NOT NULL,
+`groupId` int(11) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB ;
 '
 );
 
