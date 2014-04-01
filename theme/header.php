98d19943e4f2c26988984a05c5fd27e486b3443a
MathiasB <mathias.beke@student.uantwerpen.be>
2014-04-01 18:35:33 +0200

    'Activated' page titles

--- theme/header.php
+++ theme/header.php
@@ -9,7 +9,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Soccer Betting System</title>
+	<title><?php echo $controller->title; ?></title>
 
 	<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
