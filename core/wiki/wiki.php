b8e70241a153dad2338aa56552cdf927373c1b84
eli <eliasvanlangenhove@hotmail.com>
2014-04-02 23:22:57 +0200

    Wiki works for most players, except for some very weird exceptions.

---
+++ core/wiki/wiki.php
@@ -0,0 +1,147 @@
+<?php 
+require_once(dirname(__FILE__) . '/wikidrain/api.php');
+require_once(dirname(__FILE__) . '/../simple_html_dom.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+class Wiki {
+
+	private $site;
+	private $article;
+	private $sections;
+	private $wiki;
+
+	public function __construct() {
+
+		$this->site = "http://en.wikipedia.org";
+		$this->sections = "About";
+		$this->wiki = new wikidrain("wikidrain/1.0 (" . "$this->site" . ")");
+
+	}
+
+	private function run() {
+
+		$this->sections = "about";
+
+		$search = $this->wiki->Search($this->article);
+
+		if(sizeof($search) == 0) {
+
+			return false;
+		}
+
+		$results = $this->wiki->getSections($this->article);		
+		if(!$this->fullpage) {
+
+			$text = $this->wiki->getText("$this->article", "{$results[0]['title']}");
+			return "$text" . "<br>";			
+		}
+
+		else {
+
+			$text = $this->wiki->getText("$this->article", "{$results[0]['title']}");
+
+			foreach ($results as $section) {
+
+				echo $this->wiki->getText("$this->article", "{$section['index']}"), "<br>";
+				$tempText = $text . $this->wiki->getText("$this->article", "{$section['index']}");
+				$text = $tempText;
+			}
+		}
+	}
+
+	private function searchCorrectArticleName() {
+
+		$searchLocation = $this->site . "/w/index.php?search=";
+		for($i = 0; $i < strlen($this->article); $i++) {
+
+			if($this->article[$i] == '_') {
+
+				$searchLocation = $searchLocation . "+";
+			} 
+
+			else {
+
+				$searchLocation = $searchLocation . $this->article[$i];
+			}
+		}
+
+		$html = $this->loadPage($searchLocation);
+
+		$searchResults = $html->find('.mw-search-results' , 0);
+		$searchResultElement = $searchResults->first_child()->first_child()->first_child();
+		$this->article = $searchResultElement->getAttribute('title');
+		echo $this->article;
+	}
+
+
+
+	/**
+	Load a page, but take the one from the cache when already loaded.
+
+	@param url of the page
+
+	@return DOM object
+	*/
+	private function loadPage($url) {
+
+		$page = file_get_contents($url);
+		return str_get_html($page);
+	}
+
+	/**
+	Get the text of the player's wiki page
+
+	@param playerName The player's name
+	@param $fullpage Get the full page or just the intro. Default value is false (get just the intro)
+
+	@returns wiki page in text format, without references, external links and images
+
+	*/
+	public function getPlayerWiki($playerName, $fullpage = false) {
+
+		for($i = 0; $i < strlen($playerName); $i++) {
+
+			if($playerName[$i] == " ") {
+
+				$this->article = $this->article . "_";
+			}
+
+			else {
+
+				$this->article = $this->article . $playerName[$i];
+			}
+		}
+
+		echo utf8_decode($this->article);
+
+		$this->fullpage = $fullpage;
+		$wiki = $this->run();
+
+
+		if($wiki == false) {
+
+			$this->searchCorrectArticleName();
+			$wiki = $this->run();
+			if($wiki != false) {
+
+				return $wiki;
+			}
+
+			else{
+
+				return "Player does not have wiki page";
+			}
+
+		}
+
+		else {
+			return $wiki;
+		}
+	}		
+}
+
+$wi = new Wiki();
+echo utf8_decode($wi->getPlayerWiki("José de Jesús Corona Rodríguez"));
+?>
---
+++ core/wiki/wikidrain/README.md
@@ -0,0 +1,89 @@
+wikidrain
+===================================================================================
+
+A PHP wrapper for the WikiMedia API centralized around querying Wikipedia articles.
+
+There are many php classes, indeed complete frameworks, dedicated to the WikiMedia
+API. But none of them are easy to implement, are accessible to those just learning
+PHP, or focus on simply querying data from Wikipedia.  The goal of the wikidrain 
+project is to develop a production-quality PHP library dedicated to easy, simple, 
+and efficient querying of Wikipedia articles.
+
+
+Usage:
+===================================================================================
+
+wikidrain is very simple to use, just make a new instance of wikidrain:
+
+```php
+<?php
+    $wiki = new wikidrain('wikidrain/1.0 (http://www.example.com/)');
+?>
+```
+
+NOTE: You must supply your user-agent info, here the example header is:
+      'wikidrain/1.0 (http://www.example.com/)'
+
+To search wikipedia for articles, use the Search method:
+This returns a multidimensional array...
+
+```php
+<?php
+    $wiki = new wikidrain('wikidrain/1.0 (http://www.example.com/)');
+    $result = $wiki->Search('API');
+?>
+```
+
+To get the sections of a wikipedia page, use the getSections method:
+This also returns a multidimensional array...
+
+```php
+<?php
+    $wiki = new wikidrain('wikidrain/1.0 (http://www.example.com/)');
+    $result = $wiki->getSections('API');
+?>
+```
+
+NOTE: Wikidrain does not include any error checking/verification that the article
+      exists, so be sure to only request the sections/text of articles that were
+      returned by the Search() method...
+
+To get the text of a section from a wikipedia page, use the getText
+method.
+This returns text...
+
+```php
+<?php
+    $wiki = new wikidrain('wikidrain/1.0 (http://www.example.com/)');
+    $result = $wiki->getText('API', '0');
+?>
+```
+
+NOTE: Again, wikidrain does not include any error checking/verification to determine
+      if an article or section exists, so make sure to only request data from pages/
+      sections returned by the Search() and getSections() methods...
+
+To get the list of articles listed in the 'See also' section, use the getRelated
+method
+This returns an array...
+
+```php
+<?php
+    $wiki = new wikidrain('wikidrain/1.0 (http://www.example.com/)');
+    $wiki->getRelated('API');
+?>
+```
+
+
+TODO:
+===================================================================================
+
+Currently, the objectives are as follows:
+
+1. Anything to put less strain on the wikipedia servers
+
+2. A method to return the wikipedia donation banner
+
+3. Error checking
+
+4. Any improvement to the parseText method
