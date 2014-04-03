3bdcf4451844abefa21f87d44a832833e32f4364
MathiasB <mathias.beke@student.uantwerpen.be>
2014-04-03 09:07:56 +0200

    Basis 'Country' page controller

---
+++ theme/country.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template part for player page
+
+Created: February 2014
+*/
+?>
+<div class="container">
+
+
+	<div class="row">
+
+		
+	
+			<div class="container">
+				<h2 id="title-country"><?php echo $this->country->getName(); ?></h2>
+			</div>
+			
+	
+	
+	</div>
+	
+	
+	
+	
+	
+	
+
+
+</div>
