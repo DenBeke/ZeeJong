76e90181b5434bebb3ed803978a27257d57771db
MathiasB <bekemathias@gmail.com>
2014-04-17 20:15:26 +0200

    Added coaches to country page

--- core/controller/Country.php
+++ core/controller/Country.php
@@ -19,6 +19,7 @@ require_once(dirname(__FILE__) . '/Error.php');
 		public $title;
 		public $teams;
 		public $referees;
+		public $coaches;
 
 
 		public function __construct() {
@@ -42,6 +43,7 @@ require_once(dirname(__FILE__) . '/Error.php');
 			$this->country = $database->getCountryById($args[1]);
 			$this->teams = $database->getTeamsInCountry($this->country->getId());
 			$this->referees = $database->getRefereesInCountry($this->country->getId());
+			$this->coaches = $database->getCoachesInCountry($this->country->getId());
 
 			$this->title = 'Country - ' . $this->country->getName() . ' - ' . Controller::siteName;
 		}
--- theme/country.php
+++ theme/country.php
@@ -13,48 +13,71 @@ Created: February 2014
 		
 	
 			<div class="container">
+				
+				
 				<h2 id="title-country"><?php echo $this->country->getName(); ?></h2>
+			
+			
+			
+			
+				<!-- Teams -->
+				<h3>Teams</h3>
+				
+				<ul>
+					
+					<?php foreach ($this->teams as $team) { ?>
+					
+					<li>
+						<a href="<?php echo SITE_URL . 'team/' . $team->getId(); ?>">
+							<?php echo $team->getName(); ?>
+						</a>
+					</li>
+					
+					<?php } ?>
+				
+				</ul>
+				<!-- Teams -->
+				
+				
+				
+				<!-- Referees -->
+				<h3>Referees</h3>
+				
+				<ul>
+					
+					<?php foreach ($this->referees as $ref) { ?>
+					
+					<li>
+						<a href="<?php echo SITE_URL . 'referee/' . $ref->getId(); ?>">
+							<?php echo $ref->getName(); ?>
+						</a>
+					</li>
+					
+					<?php } ?>
+				
+				</ul>
+				<!-- Referees -->
+				
+				
+				<!-- Coaches -->
+				<h3>Coaches</h3>
+				
+				<ul>
+					
+					<?php foreach ($this->coaches as $coach) { ?>
+					
+					<li>
+						<a href="<?php echo SITE_URL . 'coach/' . $coach->getId(); ?>">
+							<?php echo $coach->getName(); ?>
+						</a>
+					</li>
+					
+					<?php } ?>
+				
+				</ul>
+				<!-- Coaches -->
+				
 			</div>
-			
-			
-			
-			<!-- Teams -->
-			<h2>Teams</h2>
-			
-			<ul>
-				
-				<?php foreach ($this->teams as $team) { ?>
-				
-				<li>
-					<a href="<?php echo SITE_URL . 'team/' . $team->getId(); ?>">
-						<?php echo $team->getName(); ?>
-					</a>
-				</li>
-				
-				<?php } ?>
-			
-			</ul>
-			<!-- Teams -->
-			
-			
-			
-			<!-- Referees -->
-			<h2>Referees</h2>
-			
-			<ul>
-				
-				<?php foreach ($this->referees as $ref) { ?>
-				
-				<li>
-					<a href="<?php echo SITE_URL . 'referee/' . $ref->getId(); ?>">
-						<?php echo $ref->getName(); ?>
-					</a>
-				</li>
-				
-				<?php } ?>
-			
-			</ul>
-			<!-- Referees -->
 			
 	
 	
