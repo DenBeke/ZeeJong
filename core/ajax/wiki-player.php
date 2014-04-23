e4b2cbc2b5d49ab359f77467b865235b0afa5492
MathiasB <bekemathias@gmail.com>
2014-04-23 12:36:18 +0200

    Added forgotten file

---
+++ core/ajax/wiki-player.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Returns JSON objects for the 'News' page.
+
+Created February 2014
+*/
+
+require_once(dirname(__FILE__) . '/../controller/Player.php');
+require_once(dirname(__FILE__) . '/../database.php');
+
+//Create database
+$database = new Database;
+
+
+
+$controller = new \Controller\Player;
+
+
+
+if(isset($_GET['player'])) {
+
+	if(intval($_GET['player'])) {
+		$controller->GET([1 => $_GET['player']]);
+		$output['wiki'] = $controller->getWiki();
+	}
+	else {
+
+		$output['error'] = 'Player does not exist';
+
+	}
+
+
+}
+else {
+	$output['error'] = 'No player id given';
+}
+
+
+
+if(isset($_GET['debug'])) {
+	echo '<pre>' . json_encode($output, JSON_PRETTY_PRINT) . '</pre>';
+}
+else {
+	echo json_encode($output, JSON_PRETTY_PRINT);
+}
+
+
+?>
+
