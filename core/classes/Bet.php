597e7ccc0aca8b2e64648e5f2f9cb97aedbd95fc
Alexander <liopleurodon@bitbucket.org>
2014-05-12 09:43:00 +0200

    Added 'hidden' bets on group page

--- core/classes/Bet.php
+++ core/classes/Bet.php
@@ -2,195 +2,241 @@
 require_once (dirname(__FILE__) . '/../database.php');
 
 /**
-@brief Class containing a bet
+ @brief Class containing a bet
 
-The class contains the following information:
-	-id 
- 	-matchId
- 	-teamId
- 	-time
- 	-userId
-*/
+ The class contains the following information:
+ -id
+ -matchId
+ -teamId
+ -time
+ -userId
+ */
 
 class Bet {
 	private $db;
 	private $id;
 
 	/**
-	Constructor
+	 Constructor
 
-	@param id The ID of the bet
-	*/
+	 @param id The ID of the bet
+	 */
 	public function __construct($id) {
-		$this->db = new Database();
-		$this->id = $id;
+		$this -> db = new Database();
+		$this -> id = $id;
 
 	}
 
 	/**
-	Returns the id
+	 Returns the id
 
-	@return id
-	*/
+	 @return id
+	 */
 	public function getId() {
-		return $this->id;
+		return $this -> id;
 	}
 
 	/**
 	 Returns the Match ID
 	 @return Match ID
 	 */
-	 public function getMatchId() {
-	 	return $this->db->getMatchFromBet($this->id);
-	 }
-	 
-	 /**
-	  Returns the User ID
-	  
-	  @return User ID
-	  */
-	  public function getUserId() {
-	  	return $this->db->getUserFromBet($this->id);
-	  }
-	  
-	  /**
-	   Returns the score put on the first team
-	    
-	   @return score team A
-	   */
-	   public function getScoreA() {
-	   	return $this->db->getScoreAFromBet($this->id);
-	   }
-	   
-	   /**
-	   Returns the score put on the second team
-	    
-	   @return score team B
-	   */
-	   public function getScoreB() {
-	   	return $this->db->getScoreBFromBet($this->id);
-	   }
-	   
-	   
-	   /**
-	   Returns the id of the player that will make the first goal
-	    
-	   @return id of player
-	   */
-	   public function getFirstGoal() {
-	   	return $this->db->getFirstGoalFromBet($this->id);
-	   }
-	   
-	   /**
-	   Returns the red cards that will be handed this game
-	    
-	   @return # red cards
-	   */
-	   public function getRedCards() {
-	   	return $this->db->getRedCardsFromBet($this->id);
-	   }
-	   
-	   /**
-	   Returns the yellow cards that will be handed this game
-	    
-	   @return # yellow cards
-	   */
-	   public function getYellowCards() {
-	   	return $this->db->getYellowCardsFromBet($this->id);
-	   }
-	   
-	   /**
-	    Returns the amount of money the bet contains
-	     
-	    @return amount of money
-	    */
-	    public function getMoney() {
-	    	return $this->db->getMoneyFromBet($this->id);
-	    }
-		
-		/**
-		 Returns the amount of items the user has bet on
-		 @return an int telling on how many options the user has bet
-		 */
-		 public function howManyItemsBet(){
-		 	$retVal=0;
-			if($this->getScoreA()>=0){
-				$retVal = $retVal +1;
-			}
-			if($this->getScoreB()>=0){
-				$retVal = $retVal +1;
-			}
-			if($this->getFirstGoal()>=0){
-				$retVal = $retVal +1;
-			}
-			if($this->getRedCards()>=0){
-				$retVal = $retVal +1;
-			}
-			if($this->getYellowCards()>=0){
-				$retVal = $retVal +1;
-			}
-			return $retVal;
-		 }
+	public function getMatchId() {
+		return $this -> db -> getMatchFromBet($this -> id);
+	}
+
+	/**
+	 Returns the User ID
+
+	 @return User ID
+	 */
+	public function getUserId() {
+		return $this -> db -> getUserFromBet($this -> id);
+	}
+	
+	/**
+	 Returns the User name
+
+	 @return User name
+	 */
+	public function getUserName() {
+		return $this -> db -> getUserName($this->getUserId());
+	}
+
+	/**
+	 Returns the score put on the first team
+
+	 @return score team A
+	 */
+	public function getScoreA() {
+		return $this -> db -> getScoreAFromBet($this -> id);
+	}
+
+	/**
+	 Returns the score put on the second team
+
+	 @return score team B
+	 */
+	public function getScoreB() {
+		return $this -> db -> getScoreBFromBet($this -> id);
+	}
+
+	/**
+	 Returns the id of the player that will make the first goal
+
+	 @return id of player
+	 */
+	public function getFirstGoal() {
+		return $this -> db -> getFirstGoalFromBet($this -> id);
+	}
+
+	/**
+	 Returns the red cards that will be handed this game
+
+	 @return # red cards
+	 */
+	public function getRedCards() {
+		return $this -> db -> getRedCardsFromBet($this -> id);
+	}
+
+	/**
+	 Returns the yellow cards that will be handed this game
+
+	 @return # yellow cards
+	 */
+	public function getYellowCards() {
+		return $this -> db -> getYellowCardsFromBet($this -> id);
+	}
+
+	/**
+	 Returns the amount of money the bet contains
+
+	 @return amount of money
+	 */
+	public function getMoney() {
+		return $this -> db -> getMoneyFromBet($this -> id);
+	}
+
+	/**
+	 Returns the amount of items the user has bet on
+	 @return an int telling on how many options the user has bet
+	 */
+	public function howManyItemsBet() {
+		$retVal = 0;
+		if ($this -> getScoreA() >= 0) {
+			$retVal = $retVal + 1;
+		}
+		if ($this -> getScoreB() >= 0) {
+			$retVal = $retVal + 1;
+		}
+		if ($this -> getFirstGoal() >= 0) {
+			$retVal = $retVal + 1;
+		}
+		if ($this -> getRedCards() >= 0) {
+			$retVal = $retVal + 1;
+		}
+		if ($this -> getYellowCards() >= 0) {
+			$retVal = $retVal + 1;
+		}
+		return $retVal;
+	}
 
 	/**
 	 Return the bet's data in string version for in a table row so it can be added easily to an existing table
 	 @return the bet's data as a string
 	 */
-	 public function dataAsString(){
+	public function dataAsString() {
 		$output = "";
-		$output = $output."<td>".$this->db->getMatchById($this->getMatchId())->getTeamA()->getName()."</td>";
-		$output = $output."<td><span class='badge'>";
-		if($this->getScoreA()!=-1){
-			$output = $output . $this->getScoreA() . " - ";
-		}else{
+		$output = $output . "<td>" . $this -> db -> getMatchById($this -> getMatchId()) -> getTeamA() -> getName() . "</td>";
+		$output = $output . "<td><span class='badge'>";
+		if ($this -> getScoreA() != -1) {
+			$output = $output . $this -> getScoreA() . " - ";
+		} else {
 			$output = $output . " / " . " - ";
 		}
-		if($this->getScoreB()!=-1){
-			$output = $output . $this->getScoreB();
-		}else{
+		if ($this -> getScoreB() != -1) {
+			$output = $output . $this -> getScoreB();
+		} else {
 			$output = $output . " / ";
 		}
-		$output = $output."</span></td>";
-		$output = $output."<td>".$this->db->getMatchById($this->getMatchId())->getTeamB()->getName()."</td>";	
-		
-		if($this->getFirstGoal()!=-1){
-			$output = $output."<td>". $this->db->getPlayerById($this->getFirstGoal())->getName()."</td>";
-		}else{
-			$output = $output."<td> / </td>";
+		$output = $output . "</span></td>";
+		$output = $output . "<td>" . $this -> db -> getMatchById($this -> getMatchId()) -> getTeamB() -> getName() . "</td>";
+
+		if ($this -> getFirstGoal() != -1) {
+			$output = $output . "<td>" . $this -> db -> getPlayerById($this -> getFirstGoal()) -> getName() . "</td>";
+		} else {
+			$output = $output . "<td> / </td>";
 		}
-		if($this->getRedCards()!=-1){
-			$output = $output . "<td>".$this->getRedCards()."</td>";
-		}else{
-			$output = $output."<td> / </td>";
+		if ($this -> getRedCards() != -1) {
+			$output = $output . "<td>" . $this -> getRedCards() . "</td>";
+		} else {
+			$output = $output . "<td> / </td>";
 		}
-		
-		if($this->getYellowCards()!=-1){
-			$output = $output . "<td>".$this->getYellowCards()."</td>";
-		}else{
-			$output = $output."<td> / </td>";
+
+		if ($this -> getYellowCards() != -1) {
+			$output = $output . "<td>" . $this -> getYellowCards() . "</td>";
+		} else {
+			$output = $output . "<td> / </td>";
 		}
-		
-		$output = $output."<td>"."€ ".$this->getMoney()."</td>";
-		
-		
-		
-		
+
+		$output = $output . "<td>" . "€ " . $this -> getMoney() . "</td>";
 		return $output;
-	 }
-	 
+	}
 
-    /**
-    String function
+	/**
+	 Return the 'hidden' data as string: this is what we want to be displayed on the groups page for the unhandled bets
 
-    @return string
-    */
-    public function __toString() {
-        $output = "ID: $this->id";
-        /*$output = $output . "$this->teamA";
-        $output = $output . " - ";
-        $output = $output . "$this->teamB";*/
-        return $output;
-    }
+	 @return a string with table rows consisting of the bet while hiding its data
+	 */
+	public function dataHiddenAsString() {
+		$output = "<td>".$this->getUserName()."</td>";
+		$output = $output . "<td>" . $this -> db -> getMatchById($this -> getMatchId()) -> getTeamA() -> getName() . "</td>";
+		$output = $output . "<td><span class='badge'>";
+		if ($this -> getScoreA() != -1) {
+			$output = $output . "???" . " - ";
+		} else {
+			$output = $output . " / " . " - ";
+		}
+		if ($this -> getScoreB() != -1) {
+			$output = $output . "???";
+		} else {
+			$output = $output . " / ";
+		}
+		$output = $output . "</span></td>";
+		$output = $output . "<td>" . $this -> db -> getMatchById($this -> getMatchId()) -> getTeamB() -> getName() . "</td>";
+
+		if ($this -> getFirstGoal() != -1) {
+			$output = $output . "<td>" . "???" . "</td>";
+		} else {
+			$output = $output . "<td> / </td>";
+		}
+		if ($this -> getRedCards() != -1) {
+			$output = $output . "<td>" ."???" . "</td>";
+		} else {
+			$output = $output . "<td> / </td>";
+		}
+
+		if ($this -> getYellowCards() != -1) {
+			$output = $output . "<td>" . "???" . "</td>";
+		} else {
+			$output = $output . "<td> / </td>";
+		}
+
+		$output = $output . "<td>" . "€ " . $this -> getMoney() . "</td>";
+		return $output;
+	}
+
+	/**
+	 String function
+
+	 @return string
+	 */
+	public function __toString() {
+		$output = "ID: $this->id";
+		/*$output = $output . "$this->teamA";
+		 $output = $output . " - ";
+		 $output = $output . "$this->teamB";*/
+		return $output;
+	}
+
 }
-
 ?>
