96c80caa5401edb985ffa73f4f37e1c6091b8f94
MathiasB <bekemathias@gmail.com>
2014-05-13 15:05:48 +0200

    Started working on admin dashboard

---
+++ theme/admin.php
@@ -0,0 +1,13 @@
+<div class="container">
+	
+	<h2>Admin</h2>
+	
+	
+	<ul class="nav nav-pills">
+	  <li class="active"><a href="<?php echo SITE_URL . 'admin/dashboard/'; ?>">Dashboard</a></li>
+	  <li><a href="#">Matches</a></li>
+	  <li><a href="#">Pages</a></li>
+	</ul>
+
+	
+</div>
