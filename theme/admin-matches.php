1e403c86a2d9609413e170687a5a639b9a7cf911
MathiasB <bekemathias@gmail.com>
2014-05-14 22:01:43 +0200

    Restyled admin matches page

--- theme/admin-matches.php
+++ theme/admin-matches.php
@@ -1,18 +1,28 @@
 <?php include(dirname(__FILE__) . '/admin.php'); ?>
 
+<p></p>
+
 <div class="container">
 
 	<?php foreach($this->competitions as $competition) { ?>
-	<div class="competitions">
-		<h5 class="hidden-click"><?php echo $competition->getName(); ?></h5>
+	<div class="competitions panel panel-default">
+		<div class="panel-heading hidden-click">
+			<h3 class="panel-title"><?php echo $competition->getName(); ?></h3>
+		</div>
 		
-		<div class="competition-content hidden-content">
+		<div class="competition-content hidden-content panel-body">
+			
 			
 			<?php foreach($competition->getTournaments() as $tournament) { ?>
-			<div class="">
-				<p class="hidden-click tournament-click"><?php echo $tournament->getName(); ?></p>
+			<div class="panel panel-default">
 				
-				<div class="hidden-content" data-url="<?php echo SITE_URL . 'tournament/' . $tournament->getId(); ?>">
+				<div class="panel-heading hidden-click tournament-click">
+				
+					<h3 class="panel-title"><?php echo $tournament->getName(); ?></h3>
+					
+				</div>
+				
+				<div class="hidden-content panel-body" data-url="<?php echo SITE_URL . 'tournament/' . $tournament->getId(); ?>">
 					
 						<div class="loader-container"><div class="loader"></div>loading</div>	
 										
--- theme/tournament.php
+++ theme/tournament.php
@@ -80,11 +80,11 @@ Created: February 2014
 			
 			
 			<td class="admin-only">
-				<a href="<?php echo SITE_URL . 'admin/match/' . $match->getId() . '/edit'; ?>" class="btn-xs btn btn-default">Edit</a>
+				<a href="<?php echo SITE_URL . 'admin/match/' . $match->getId() . '/edit'; ?>" class="btn-xs btn btn-primary">Edit</a>
 			</td>
 			
 			<td class="admin-only">
-				<a href="<?php echo SITE_URL . 'admin/match/' . $match->getId() . '/delete'; ?>" class="btn-xs btn btn-default">Delete</a>
+				<a href="<?php echo SITE_URL . 'admin/match/' . $match->getId() . '/delete'; ?>" class="btn-xs btn btn-danger">Delete</a>
 			</td>
 			
 		</tr>
