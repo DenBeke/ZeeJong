32b8109068c267181758f3f4b047a7f7bf0e052f
Alexander <liopleurodon@bitbucket.org>
2014-05-15 15:59:31 +0200

    Finished admin users panel

--- theme/admin-users.php
+++ theme/admin-users.php
@@ -1,22 +1,49 @@
-<?php include(dirname(__FILE__) . '/admin.php'); ?>
+<?php
+include (dirname(__FILE__) . '/admin.php');
+ ?>
 
-
+<div class="container">
 <?php
 
 if(isAdmin()){
-?>
+	
+	
+	if(strlen($this-> getErrorMessage())>0){
+	// there were errors, let's display them
+	?>
+	<div class="alert alert-danger">
+		<strong> <?php
+		echo nl2br($this -> getErrorMessage());
+		?></strong>
+	</div>
 
-<div class="container">
+	<?php
+	}
+	if(strlen($this->getSuccessMessage())>0){
+	// at least one thing succeeded, let's display what
+	?>
+	<div class="alert alert-success">
+		<strong> <?php
+		echo nl2br($this -> getSuccessMessage());
+		?></strong>
+	</div>
+	<?php
+	}
+	// Display 'change settings' form
+	?>
+	
+
+
 <div class="col-md-5">
 <h3>Ban a user</h3>
 <div class="well">
-	<form id="signup" class="form-horizontal" role="form" method="post" action="<?php echo SITE_URL ?>admin-users">
+	<form id="signup" class="form-horizontal" role="form" method="post" action="<?php echo SITE_URL ?>admin/users">
 		<div class="form-group">
 			<label class="control-label col-sm-2">User name</label>
 			<div class="controls col-sm-10">
 				<div class="input-group">
 					<span class="add-on input-group-addon"> <span class="glyphicon glyphicon-user"></span> </span>
-					<input type="text" class="form-control input-xlarge" id="name" name="name" placeholder="name">
+					<input type="text" class="form-control input-xlarge" id="nameBan" name="nameBan" placeholder="name">
 				</div>
 			</div>
 		</div>
@@ -36,13 +63,13 @@ if(isAdmin()){
 
 <h3>Make a user admin</h3>
 <div class="well">
-	<form id="signup" class="form-horizontal" role="form" method="post" action="<?php echo SITE_URL ?>admin-users">
+	<form id="signup" class="form-horizontal" role="form" method="post" action="<?php echo SITE_URL ?>admin/users">
 		<div class="form-group">
 			<label class="control-label col-sm-2">User name</label>
 			<div class="controls col-sm-10">
 				<div class="input-group">
 					<span class="add-on input-group-addon"> <span class="glyphicon glyphicon-user"></span> </span>
-					<input type="text" class="form-control input-xlarge" id="name" name="name" placeholder="name">
+					<input type="text" class="form-control input-xlarge" id="nameMakeAdmin" name="nameMakeAdmin" placeholder="name">
 				</div>
 			</div>
 		</div>
@@ -60,13 +87,13 @@ if(isAdmin()){
 
 <h3>Take admin rights from user</h3>
 <div class="well">
-	<form id="signup" class="form-horizontal" role="form" method="post" action="<?php echo SITE_URL ?>admin-users">
+	<form id="signup" class="form-horizontal" role="form" method="post" action="<?php echo SITE_URL ?>admin/users">
 		<div class="form-group">
 			<label class="control-label col-sm-2">User name</label>
 			<div class="controls col-sm-10">
 				<div class="input-group">
 					<span class="add-on input-group-addon"> <span class="glyphicon glyphicon-user"></span> </span>
-					<input type="text" class="form-control input-xlarge" id="name" name="name" placeholder="name">
+					<input type="text" class="form-control input-xlarge" id="nameRemoveAdmin" name="nameRemoveAdmin" placeholder="name">
 				</div>
 			</div>
 		</div>
