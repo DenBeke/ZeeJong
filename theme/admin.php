9d3b63106075837c492d1fbae16a17b3a792618a
Alexander <liopleurodon@bitbucket.org>
2014-05-15 14:51:13 +0200

    Added emtpy admin user pages

--- theme/admin.php
+++ theme/admin.php
@@ -7,7 +7,7 @@
 	  <li class="<?php if($this->page == 'admin-dashboard') echo 'active'; ?>"><a href="<?php echo SITE_URL . 'admin/dashboard/'; ?>">Dashboard</a></li>
 	  <li class="<?php if($this->page == 'admin-matches') echo 'active'; ?>"><a href="<?php echo SITE_URL . 'admin/matches/'; ?>">Matches</a></li>
 	  <li class="<?php if($this->page == 'admin-pages') echo 'active'; ?>"><a href="<?php echo SITE_URL . 'admin/pages/'; ?>">Pages</a></li>
-	  <li><a href="#">Users</a></li>
+	  <li class="<?php if($this->page == 'admin-users') echo 'active'; ?>"><a href="<?php echo SITE_URL . 'admin/users/'; ?>">Users</a></li>
 	</ul>
 
 	
