6abb2f043ea8e27329f9e09bc68cef5f4d7fe732
MathiasB <bekemathias@gmail.com>
2014-05-20 14:44:48 +0200

    Enhanced style of bet updater

--- theme/admin-update-bets.php
+++ theme/admin-update-bets.php
@@ -3,7 +3,7 @@ include (dirname(__FILE__) . '/admin.php');
 $this->update();
 ?>
 <div class="container">
-<div class="alert-success">
+<div class="alert alert-success">
 	<h2>Successfully processed bets</h2>
 	<h3>Total bets processed: <?php echo $this->getBetsProcessed() ?></h3>
 	<h3>Money distributed: <?php echo $this->getMoneyDistributed() ?></h3>
